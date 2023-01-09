59dc5bb1054364b9fb8ec1546486a43e1a53b8f3
Osvaldo <torquati.osvaldo@gmail.com>
2023-01-08 21:29:26 -0300

    Create Aula004.py
    Código referente a aula 04 e resolução dos desafios.

---
+++ Python-Aulas/Aula004.py
@@ -0,0 +1,20 @@
+print('Olá, Mundo!')
+print(7+4)
+print('7'+'4')
+print('Olá',5)
+# Uso de variáveis
+# Desafio 01
+nome = input('Qual seu nome: ')
+print('Olá,', nome, ' Prazer em conhece-lo!')
+# Desafio 02
+print('Informe a data do teu nascimento')
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+print('Olá, {}, você nasceu em {} de {} de {}, correto?'.format(nome, dia, mes, ano))
+# Desafio 03
+print('Somando numeros')
+n1 = input('digite um número: ')
+n2 = input('digite outro número: ')
+soma = int(n1)+int(n2)
+print('A soma de {} mais {} é igual a: {}'.format(n1,n2,soma))
